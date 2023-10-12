d2ccac02313f561504723a8ddb8466cdd4c12b56
FebScap <vanhorlenicolas@gmail.com>
2023-10-12 14:50:18 +0200

    new

---
+++ formulaires/boulangerie.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Poste Zéphyr</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sanchez:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>POSTE ZÉPHYR</h1>
+        <nav>
+            <ul>
+                <li><a href="#accueil" class="nav_btn">Accueil</a></li>
+                <li><a href="#boulangerie" class="nav_btn">Boulangerie</a></li>
+                <li><a href="#cinema" class="nav_btn">Cinéma</a></li>
+                <li><a href="#sapins" class="nav_btn">Sapins</a></li>
+                <li><a href="#contact" class="nav_btn">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <section id="accueil">
+        <div class="accueil">
+            <span class="element">
+                <img class="logo" alt="Logo Poste Zéphyr" src="img/logo.png">
+            </span>
+            <span class="element">
+                <h2>Bienvenue sur le site officiel du Poste Zéphyr de l'unité Saint-François d'asise</h2>
+                <p>Vous trouverez ici toutes les informations concernant notre futur camp ainsi que nos actions visant à financer celui-ci.</p>
+                <p>Nos actions seront, cette année, toutes accessibles ici. Vous pourrez donc commander ou reserver soit dirrectement via les pionniers, soit via notre site.</p>
+            </span>
+        </div>
+    </section>
+
+    <footer>
+        <p>&copy; 2023 Nicolas Vanhorle</p>
+    </footer>
+</body>
+</html>
